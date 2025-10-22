9edfbf33a7d6dfd9cc9ce490324fadae772e8671
Gabriel Ravelo <ravelogabriel2@gmail.com>
2025-10-21 23:18:00 -0400

    creating provider and adding pagination in pokemon seed

--- 03-pokedex/src/pokemon/pokemon.module.ts
+++ 03-pokedex/src/pokemon/pokemon.module.ts
@@ -5,15 +5,16 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Pokemon, PokemonSchema } from './entities/pokemon.entity';
 
 @Module({
-  controllers: [PokemonController],
-  providers: [PokemonService],
-  imports: [
-	MongooseModule.forFeature([
-		{
-			name: Pokemon.name,
-			schema: PokemonSchema,
-		}
-	]),
-  ]
+    controllers: [PokemonController],
+    providers: [PokemonService],
+    imports: [
+        MongooseModule.forFeature([
+            {
+                name: Pokemon.name,
+                schema: PokemonSchema,
+            },
+        ]),
+    ],
+    exports: [MongooseModule],
 })
 export class PokemonModule {}
