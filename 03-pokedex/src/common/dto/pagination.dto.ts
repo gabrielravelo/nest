e87ccb0fc5cd90576217d98930e2ee4928fc590e
Gabriel Ravelo <gravelo@ks2.tech>
2025-10-22 17:54:40 -0400

    pokedex finished

---
+++ 03-pokedex/src/common/dto/pagination.dto.ts
@@ -0,0 +1,17 @@
+import { IsPositive, IsOptional, IsNumber, Min } from 'class-validator';
+
+export class PaginationDto {
+    
+    
+    @IsOptional()
+    @IsPositive()
+    @IsNumber()
+    @Min(1)
+    limit?: number;
+
+    @IsOptional()
+    @IsPositive()
+    @IsNumber()
+    offset?: number;
+}
+
--- 03-pokedex/src/main.ts
+++ 03-pokedex/src/main.ts
@@ -3,17 +3,21 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api/v1');
+    app.setGlobalPrefix('api/v1');
 
-  app.useGlobalPipes(
-	new ValidationPipe({
-		whitelist: true,
-		forbidNonWhitelisted: true,
-	})
-  );
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+            forbidNonWhitelisted: true,
+            transform: true,
+            transformOptions: {
+                enableImplicitConversion: true,
+            }
+        }),
+    );
 
-  await app.listen(process.env.PORT ?? 3000);
+    await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
