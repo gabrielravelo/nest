c269fb3889bff8709c6a44feea2be5f2a7a56e97
Gabriel Ravelo <ravelogabriel2@gmail.com>
2025-11-09 21:04:47 -0400

    TypeORM Finished (with relations)🚀

---
+++ 04-teslo-shop/src/files/files.controller.ts
@@ -0,0 +1,28 @@
+import {
+    Controller,
+    Get,
+    Post,
+    Body,
+    Patch,
+    Param,
+    Delete,
+    UploadedFile,
+    ParseFilePipe,
+    UseInterceptors,
+} from '@nestjs/common';
+import { FilesService } from './files.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+@Controller('files')
+export class FilesController {
+    constructor(private readonly filesService: FilesService) {}
+
+    @Post('/product')
+    @UseInterceptors(FileInterceptor('file'))
+    uploadProductImage(
+        @UploadedFile('file', ParseFilePipe)
+        file: Express.Multer.File 
+    ) {
+        return file;
+    }
+}
--- 04-teslo-shop/src/seed/seed.service.ts
+++ 04-teslo-shop/src/seed/seed.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { ProductsService } from 'src/products/products.service';
+import { initialData } from './data/seed-data';
 
 @Injectable()
 export class SeedService {
@@ -12,7 +13,17 @@ export class SeedService {
     }
 
     private async insertNewProducts() {
-        this.productsService.deleteAllProducts();
+        await this.productsService.deleteAllProducts();
+
+        const products = initialData.products;
+
+        const insertPromises: any = [];
+
+        products.forEach( product => {
+            insertPromises.push( this.productsService.create(product) );
+        });
+
+        await Promise.all( insertPromises );
 
         return true;
     }
